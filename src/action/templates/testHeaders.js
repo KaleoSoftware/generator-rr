c39cf88f814f87d2ef4f7ba0fea1e46cfcb88c21
Tucker Connelly <web@tuckerconnelly.com>
2016-01-23 19:11:49 -0500

    Refactor actions

---
+++ src/action/templates/testHeaders.js
@@ -0,0 +1,5 @@
+import test from 'tape-catch'
+import {actionTest} from 'tape-redux'
+
+import * as types from '../../src/constants/ActionTypes'
+import * as actions from '../../src/actions/<%= group %>'
